0d7d18bc451651e3a893a3db32dab1b52bf0aea3
Rishav Jaiswal <95870745+Rishav-rj@users.noreply.github.com>
2022-12-09 23:30:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const app = express();
-// const Cors = require("Cors");
 const path = require('path');
 require('dotenv').config();
 
 app.use(express.json());
-// app.use(Cors());
 
 mongoose.connect(process.env.MONGODB,
 {
@@ -19,28 +17,11 @@ app.use("/", movieRouter);
 const userRouter = require("./Routes/user.routes");
 app.use("/user", userRouter);
 
-// app.use(express.static(path.join(__dirname, 'client/build')))
-// app.get('*', function(req,res){
-//     res.sendFile(path.join(__dirname, "client/build/index.html"));
-// });
-
-// __dirname = path.resolve();
-// if(process.env.NODE_ENV==='production'){
-//     app.use(express.static(path.join(__dirname,'/client/build')));
-
-//     app.get('*', (req,res)=>{
-//         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-//     })
-// }else{
-//     app.get('/', (req,res)=>{
-//         res.send("API is Running...");
-//     });
-// }
 app.get("/", (req, res)=>{
     res.send("Server running")
 })
 
 
-app.listen(process.env.PORT || 3003, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
     console.log("Server is running at 3000");
 })
